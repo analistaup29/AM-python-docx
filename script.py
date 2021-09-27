4c4cd15bf6fdd2f50f0e0020d1711ea01a000445
mttito <42793257+mttito@users.noreply.github.com>
2021-09-27 18:33:03 -0500

    Edición del capítulo de mascarilla
    Edición del capítulo de mascarilla

--- script.py
+++ script.py
@@ -31,12 +31,23 @@ tabla_intervenciones = data_intervenciones[["region", "cas_no_cas","intervencion
 # Eliminamos filas de "No hay Intervenciones pedagogicas"
 tabla_intervenciones = tabla_intervenciones[tabla_intervenciones['intervencion_pedagogica'] != "No hay Intervenciones Pedagógicas"]
 
+# B) Siaf de mascarillas
+## Cargamos la base insumo de mascarillas
+data_mascarillas = pd.read_excel(here() / "input/Incorporación_DU_SIAF_20210921.xlsx", sheet_name='Sheet1')
+data_mascarillas = clean_names(data_mascarillas) # Normalizamos nombres
+
+# Mantenemos variables de interés (transferencia,  CERTIFICADO, COMPROMETIDO y DEVENGADO) y 
+# colapsamos a nivel de Region y UE
+tabla_mascarillas = data_mascarillas[["region","nom_ue","certificado","comprometido_anual","devengado","transferencia"]]. \
+   groupby(by = ["region", "nom_ue"], as_index=False).sum()
+
+tabla_mascarillas["region"] = tabla_mascarillas["region"].str.split(". ", n=1).apply(lambda l: "".join(l[1]))
+
 
 # (PENDIENTE) FOR LOOP de número de intervenciones pedagógicas, Mascarillas y CDD
 numero_intervenciones = "8" ## PENDIENTE
 # Mascarillas y protectores faciales
-fecha_corte_mascarillas = "5"
-devengado_mascarillas = "11,111"
+fecha_corte_mascarillas = "21 de setiembre de 2021"
 # Compromisos de desempeño
 transferido_compromisos = "22,222"
 acciones_centrales_cdd = "88,888"
@@ -53,6 +64,14 @@ for region in lista_regiones:
     # Generamos los indicadores de PIM y ejecución de intervenciones
     pim_intervenciones_region = str('{:,.0f}'.format(tabla1_region["pim_reporte_siaf_20210923"].sum()))
     ejecucion_intervenciones_region = str('{:,.0f}'.format(tabla1_region["presupuesto_devengado_reporte_siaf_20210923"].sum()))
+    # Generamos la tabla "tabla1_mascarilla" - mantiene la región i de la lista de
+    # regiones
+    region_seleccionada = tabla_mascarillas['region'] == region
+    tabla2_region = tabla_mascarillas[region_seleccionada]
+    # Generamos los indicadores de PIM y ejecución de intervenciones
+    transferencia_mascarilla = str('{:,.1f}'.format(tabla2_region["transferencia"].sum()/1000000))
+    devengado_mascarillas=str('{:.1%}'.format(tabla2_region["devengado"].sum()/tabla2_region["transferencia"].sum()))
+    #########################################################################
     # Incluimos el código del Documento
     document = docx.Document(here() / "input/formato.docx") # Creación del documento en base al template
     title=document.add_heading('AYUDA MEMORIA', 0) #Título del documento
@@ -108,6 +127,45 @@ for region in lista_regiones:
     interv_parrafo4.add_run(ejecucion_intervenciones_region)
     interv_parrafo4.add_run(".")
     interv_parrafo4.paragraph_format.alignment = docx.enum.text.WD_ALIGN_PARAGRAPH.JUSTIFY
+    #############################################################
+    # Incluimos sección 2 Mascarillas y protectores faciales
+    document.add_heading("2. Mascarillas y protectores faciales", level=1)
+    mascarillas_parrafo1 = document.add_paragraph(
+    "Mediante el Decreto de Urgencia N° 021-2021 y la Resolución de Secretaría \
+General N° 047-2021-MINEDU, se transfirieron S/ ", style="List Bullet")
+    mascarillas_parrafo1.add_run(transferencia_mascarilla)
+    mascarillas_parrafo1.add_run(" millones de soles para \
+la adquisición y distribución de mascarillas faciales textiles de uso \
+comunitario para estudiantes y personal que labora en instituciones \
+educativas públicas, así como protectores faciales para el mencionado \
+personal.")
+    mascarillas_parrafo1.paragraph_format.alignment = docx.enum.text.WD_ALIGN_PARAGRAPH.JUSTIFY
+    mascarillas_parrafo2 = document.add_paragraph(
+    "La adquisición de mascarillas y protectores faciales es condición necesaria \
+para el retorno seguro a los servicios educativos presenciales y \
+semipresenciales, según lo dispuesto por las “Disposiciones para la \
+prestación del servicio en las instituciones y programas educativos \
+públicos y privados de la Educación Básica de los ámbitos urbanos y \
+rurales, en el marco de la emergencia sanitaria de la COVID-19”, \
+aprobado mediante Resolución Ministerial N° 121-2021- MINEDU y modificado \
+con Resoluciones Ministeriales N° 199-2021-MINEDU y N° 273-2021- MINEDU.", style="List Bullet")
+    mascarillas_parrafo2.paragraph_format.alignment = docx.enum.text.WD_ALIGN_PARAGRAPH.JUSTIFY
+    mascarillas_parrafo3 = document.add_paragraph(
+    "Con fecha de corte al ", style="List Bullet")
+    mascarillas_parrafo3.add_run(fecha_corte_mascarillas)
+    mascarillas_parrafo3.add_run(
+    ", la ejecución a nivel regional de los recursos de mascarillas faciales \
+textiles protectores faciales fue del ")
+    mascarillas_parrafo3.add_run(devengado_mascarillas)
+    mascarillas_parrafo3.add_run(" (devengado) según se presenta a continuación:")
+    mascarillas_parrafo3.paragraph_format.alignment = docx.enum.text.WD_ALIGN_PARAGRAPH.JUSTIFY
+    tabla2_interv = document.add_table(tabla2_region.shape[0]+1, tabla2_region.shape[1])
+    tabla2_interv.style = "Colorful List Accent 1"
+    for j in range(tabla2_region.shape[-1]):
+        tabla2_interv.cell(0,j).text = tabla2_region.columns[j]
+    for i in range(tabla2_region.shape[0]):
+        for j in range(tabla2_region.shape[-1]):
+            tabla2_interv.cell(i+1,j).text = str(tabla2_region.values[i,j])
     document.save(here() / f'output/{region}_AM.docx')
 
 
