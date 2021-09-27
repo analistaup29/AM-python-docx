033c67de439306b2b3e327d9f8b74e783b7f0ba2
branmora <59512596+branmora@users.noreply.github.com>
2021-09-27 12:10:26 -0500

    update for loop

--- script.py
+++ script.py
@@ -10,131 +10,217 @@ import docx
 import pandas as pd
 from datetime import datetime
 from pyprojroot import here
+from janitor import clean_names # pip install pyjanitor
 
 # Opciones --------------------------------------------------------------------
 
-#Reemplazar con un loop
-region = "Cajamarca"
-
+# Formato tablas
+pd.options.display.float_format = '${:,.0f}'.format
 
 
 # Datasets --------------------------------------------------------------------
 
+# Data
+data_intervenciones = pd.read_excel(here() / "input/equipo_data/Disponibilidad_Presupuestal_20210923interv.xlsx")
+data_intervenciones = clean_names(data_intervenciones)
+
+tabla_intervenciones = data_intervenciones[["region", "cas_no_cas","intervencion_pedagogica", "pim_reporte_siaf_20210923", "presupuesto_certificado_reporte_siaf_20210923", "comprometido_anual_reporte_siaf_20210923", "presupuesto_devengado_reporte_siaf_20210923"]]. \
+   groupby(by = ["region", "cas_no_cas", "intervencion_pedagogica"] , as_index=False).sum()
+
+tabla_intervenciones = tabla_intervenciones[tabla_intervenciones['intervencion_pedagogica'] != "No hay Intervenciones Pedagógicas"]
+
+
+# Intervenciones pedagogicas
+numero_intervenciones = "8"
+
+lista_regiones = ["AMAZONAS", "TACNA", "AREQUIPA"]
+
+for region in lista_regiones:
+    region_seleccionada = tabla_intervenciones['region'] == region
+    tabla1_region = tabla_intervenciones[region_seleccionada]
+    pim_intervenciones_region = str('{:,.0f}'.format(tabla1_region["pim_reporte_siaf_20210923"].sum()))
+    ejecucion_intervenciones_region = str('{:,.0f}'.format(tabla1_region["presupuesto_devengado_reporte_siaf_20210923"].sum()))
+    document = docx.Document(here() / "input/formato.docx") # Creación del documento en base al template
+    title=document.add_heading('AYUDA MEMORIA', 0) #Título del documento
+    run = title.add_run()
+    run.add_break()
+    title.add_run('REGIÓN ')
+    title.add_run(region)
+    run = title.add_run()
+    run.add_break()
+    title.add_run(datetime.today().strftime('%d-%m-%y'))
+    document.add_heading("1. Intervenciones pedagógicas", level=1) # 1) Intervenciones pedagógicas
+    interv_parrafo1 = document.add_paragraph(
+    "Las Unidades Ejecutoras de Educación de la región " , style="List Bullet")
+    interv_parrafo1.add_run(region)
+    interv_parrafo1.add_run(" vienen implementando ")
+    interv_parrafo1.add_run(numero_intervenciones)
+    interv_parrafo1.add_run(
+    " intervenciones y acciones pedagógicas en el Año 2021, en el marco de la \
+    Norma Técnica “Disposiciones para la implementación de las intervenciones \
+    y acciones pedagógicas del Ministerio de Educación en los Gobiernos Regionales \
+    y Lima Metropolitana en el Año Fiscal 2021”, aprobada mediante \
+    RM N° 043-2021-MINEDU y modificada RM N° 159-2021-MINEDU." )
+    interv_parrafo1.paragraph_format.alignment = docx.enum.text.WD_ALIGN_PARAGRAPH.JUSTIFY
+    tabla_interv = document.add_table(tabla1_region.shape[0]+1, tabla1_region.shape[1])
+    tabla_interv.style = "Colorful List Accent 1"
+    for j in range(tabla1_region.shape[-1]):
+        tabla_interv.cell(0,j).text = tabla1_region.columns[j]
+    for i in range(tabla1_region.shape[0]):
+        for j in range(tabla1_region.shape[-1]):
+            tabla_interv.cell(i+1,j).text = str(tabla1_region.values[i,j])
+    interv_parrafo2 = document.add_paragraph(
+    "A través de los Decretos Supremos N°s 092, 169, 189, 209 y 210-2021-EF, \
+    se realizaron todas las transferencias de partidas programadas para el Año \
+    Fiscal 2021 para el financiamiento de las intervenciones y acciones pedagógicas \
+    hasta el 31 de diciembre.", style="List Bullet")
+    interv_parrafo2.paragraph_format.alignment = docx.enum.text.WD_ALIGN_PARAGRAPH.JUSTIFY
+    interv_parrafo3 = document.add_paragraph(
+    "Es importante considerar que la ejecución en los Contratos Administrativos \
+    de Servicios (CAS) se ha visto afectada por la vigencia de la Ley N° 31131. \
+    Por otro lado, la ejecución en bienes y servicios (excluyendo CAS) es menor \
+    a lo esperado debido al bajo número de IIEE que brindan servicios presenciales \
+    y semipresenciales a nivel nacional.", style="List Bullet")
+    interv_parrafo3.paragraph_format.alignment = docx.enum.text.WD_ALIGN_PARAGRAPH.JUSTIFY
+    interv_parrafo4 = document.add_paragraph(
+    "Las Unidades Ejecutoras de Educación de la región " , style="List Bullet")
+    interv_parrafo4.add_run(region)
+    interv_parrafo4.add_run(" cuentan con ")
+    interv_parrafo4.add_run(pim_intervenciones_region)
+    interv_parrafo4.add_run(
+    " millones en su Presupuesto Institucional Modificado (PIM) para el \
+    financiamiento de intervenciones y acciones pedagógicas, de los cuales se han ejecutado S/ ")
+    interv_parrafo4.add_run(ejecucion_intervenciones_region)
+    interv_parrafo4.add_run(".")
+    interv_parrafo4.paragraph_format.alignment = docx.enum.text.WD_ALIGN_PARAGRAPH.JUSTIFY
+    document.save(here() / f'output/{region}_AM.docx')
 
 
 
-# Contenido -------------------------------------------------------------------
+# Mascarillas y protectores faciales
+fecha_corte_mascarillas = "5"
+devengado_mascarillas = "11,111"
+
+# Compromisos de desempeño
+transferido_compromisos = "22,222"
+acciones_centrales_cdd = "88,888"
 
 
-# Creación del documento en base al template
-document = docx.Document(here() / "input/formato.docx")
+# # Contenido -------------------------------------------------------------------
 
-#Título
-title=document.add_heading('AM Temas Presupuestales', 0)
-run = title.add_run()
-run.add_break()
-title.add_run('Región ')
-title.add_run(region)
-run = title.add_run()
-run.add_break()
-title.add_run(datetime.today().strftime('%d-%m-%y'))
+# # Creación del documento en base al template
+# document = docx.Document(here() / "input/formato.docx")
 
-document.add_heading('1. Financiamiento de plazas 2021', level=1)
-pa1=document.add_paragraph('A través del DS 078-2021-EF, se financiaron ', style='List Bullet')
-pa1.add_run('XXX') #Reemplazar 
-pa1.add_run(' plazas de docentes de aula en el marco de los resultados del proceso de racionalización 2020 \
-en servicios educativos públicos de la región ') 
-pa1.add_run(region) 
-pa1.add_run(' con la siguiente distribución por UGEL:') 
+# #Título del documento
+# title=document.add_heading('AYUDA MEMORIA', 0)
+# run = title.add_run()
+# run.add_break()
+# title.add_run('REGIÓN ')
+# title.add_run(region)
+# run = title.add_run()
+# run.add_break()
+# title.add_run(datetime.today().strftime('%d-%m-%y'))
 
 
-document.add_heading('2. Resultados proceso de racionalización 2020', level=1)
-pa2= document.add_paragraph('En el proceso de racionalización 2020, se identificó en la región ', style='List Bullet') 
-pa2.add_run(region).bold = True
-pa2.add_run(' un total de ').bold = True
-pa2.add_run('XXX').bold = True #Reemplazar 
-pa2.add_run(' plazas de docentes de aula excedentes y ').bold = True
-pa2.add_run('XXX').bold = True #Reemplazar 
-pa2.add_run(' plazas de requerimiento').bold = True
-pa2.add_run('. A partir de esos resultados, se procedió \
-a calcular el requerimiento y la excedencia por UGEL y el agregado \
-a nivel regional, ello se puede observar en las dos últimas columnas \
-del siguiente cuadro:')
+# # 1) Intervenciones pedagógicas
 
-pa3= document.add_paragraph('En el proceso de racionalización 2020, se identificó en la región ', style='List Bullet') 
-pa3.add_run(region).bold = True
-pa3.add_run(' un total de ')
-pa3.add_run('XXX') #Reemplazar 
-pa3.add_run(' plazas de docentes de aula excedentes y ')
-pa3.add_run('XXX') #Reemplazar 
-pa3.add_run(' plazas de requerimiento. A partir de esos resultados, se procedió a  \
-calcular el requerimiento y la excedencia por UGEL y el agregado a nivel regional,  \
-ello se puede observar en las dos últimas columnas del siguiente cuadro:') 
+# document.add_heading("1. Intervenciones pedagógicas", level=1)
 
-pa4= document.add_paragraph('Por lo tanto, a nivel regional se contaba con una brecha interna de ', style='List Bullet')
-pa4.add_run('XXX') #Reemplazar 
-pa4.add_run(' plazas en ')
-pa4.add_run('XXX') #Reemplazar 
-pa4.add_run(' UGEL, y un excedente neto de plazas vacantes ascendente a ')
-pa4.add_run('XXX') #Reemplazar 
-pa4.add_run(' plazas en ')
-pa4.add_run('XXX') #Reemplazar 
-pa4.add_run(' UGEL. Con ello, se obtuvo un requerimiento neto a nivel regional igual a ')
-pa4.add_run('XXX') #Reemplazar 
-pa4.add_run('.') 
+# interv_parrafo1 = document.add_paragraph(
+#     "Las Unidades Ejecutoras de Educación de la región" , style="List Bullet")
+# interv_parrafo1.add_run(region)
+# interv_parrafo1.add_run(" vienen implementando ")
+# interv_parrafo1.add_run(numero_intervenciones)
+# interv_parrafo1.add_run(
+#     " intervenciones y acciones pedagógicas en el Año 2021, en el marco de la \
+# Norma Técnica “Disposiciones para la implementación de las intervenciones \
+# y acciones pedagógicas del Ministerio de Educación en los Gobiernos Regionales \
+# y Lima Metropolitana en el Año Fiscal 2021”, aprobada mediante \
+# RM N° 043-2021-MINEDU y modificada RM N° 159-2021-MINEDU." )
+# interv_parrafo1.paragraph_format.alignment = docx.enum.text.WD_ALIGN_PARAGRAPH.JUSTIFY
+
+# interv_parrafo2 = document.add_paragraph(
+#     "A través de los Decretos Supremos N°s 092, 169, 189, 209 y 210-2021-EF, \
+# se realizaron todas las transferencias de partidas programadas para el Año \
+# Fiscal 2021 para el financiamiento de las intervenciones y acciones pedagógicas \
+# hasta el 31 de diciembre.", style="List Bullet")
+# interv_parrafo2.paragraph_format.alignment = docx.enum.text.WD_ALIGN_PARAGRAPH.JUSTIFY
+
+# interv_parrafo3 = document.add_paragraph(
+#     "Es importante considerar que la ejecución en los Contratos Administrativos \
+# de Servicios (CAS) se ha visto afectada por la vigencia de la Ley N° 31131. \
+# Por otro lado, la ejecución en bienes y servicios (excluyendo CAS) es menor \
+# a lo esperado debido al bajo número de IIEE que brindan servicios presenciales \
+# y semipresenciales a nivel nacional.", style="List Bullet")
+# interv_parrafo3.paragraph_format.alignment = docx.enum.text.WD_ALIGN_PARAGRAPH.JUSTIFY
+
+# interv_parrafo4 = document.add_paragraph(
+#     "Las Unidades Ejecutoras de Educación de la región " , style="List Bullet")
+# interv_parrafo4.add_run(region)
+# interv_parrafo4.add_run(" cuentan con ")
+# interv_parrafo4.add_run(pim_intervenciones)
+# interv_parrafo4.add_run(
+#     " millones en su Presupuesto Institucional Modificado (PIM) para el \
+# financiamiento de intervenciones y acciones pedagógicas, de los cuales se han ejecutado S/ ")
+# interv_parrafo4.add_run(ejecutado_intervenciones)
+# interv_parrafo4.add_run(".")
+# interv_parrafo4.paragraph_format.alignment = docx.enum.text.WD_ALIGN_PARAGRAPH.JUSTIFY
 
 
-document.add_heading('3. Acciones de reordenamiento territorial 2020', level=1)
 
-pa5= document.add_paragraph('En el marco del proceso de racionalización 2020, en la región ', style='List Bullet')
-pa5.add_run(region)
-pa5.add_run(' no se inhabilitaron plazas a pesar de contar con ')
-pa5.add_run('XX')
-pa5.add_run(' plazas vacantes identificadas como excedencia neta.') 
+# # 2) Mascarillas y protectores faciales
+
+# document.add_heading("2. Mascarillas y protectores faciales", level=1)
+
+# mascarillas_parrafo1 = document.add_paragraph(
+#     "Mediante el Decreto de Urgencia Nº 021-2021 y la Resolución de Secretaría \
+# General N° 047-2021-MINEDU, se transfirieron S/ 96.3 millones de soles para \
+# la adquisición y distribución de mascarillas faciales textiles de uso  \
+# comunitario para estudiantes y personal que labora en instituciones \
+# educativas públicas, así como protectores faciales para el mencionado \
+# personal.", style="List Bullet")
+# mascarillas_parrafo1.paragraph_format.alignment = docx.enum.text.WD_ALIGN_PARAGRAPH.JUSTIFY
+
+# mascarillas_parrafo2 = document.add_paragraph(
+#     "La adquisición de mascarillas y protectores faciales es condición necesaria \
+# para el retorno seguro a los servicios educativos presenciales y \
+# semipresenciales, según lo dispuesto por las “Disposiciones para la \
+# prestación del servicio en las instituciones y programas educativos \
+# públicos y privados de la Educación Básica de los ámbitos urbanos y \
+# rurales, en el marco de la emergencia sanitaria de la COVID-19”, \
+# aprobado mediante Resolución Ministerial N° 121-2021- MINEDU y modificado \
+# con Resoluciones Ministeriales N° 199-2021-MINEDU y N°273-2021- MINEDU.", style="List Bullet")
+# mascarillas_parrafo2.paragraph_format.alignment = docx.enum.text.WD_ALIGN_PARAGRAPH.JUSTIFY
+
+# mascarillas_parrafo3 = document.add_paragraph(
+#     "Con fecha de corte al ", style="List Bullet")
+# mascarillas_parrafo3.add_run(fecha_corte_mascarillas)
+# mascarillas_parrafo3.add_run(
+#     " la ejecución a nivel regional de los recursos de mascarillas faciales \
+#     textiles protectores faciales fue del ")
+# mascarillas_parrafo3.add_run(devengado_mascarillas)
+# mascarillas_parrafo3.add_run(" (devengado) según se presenta a continuación:")
+# mascarillas_parrafo3.paragraph_format.alignment = docx.enum.text.WD_ALIGN_PARAGRAPH.JUSTIFY
+
+# # 3) Compromisos de Desempeño
+
+# document.add_heading("3. Compromisos de Desempeño", level=1)
+
+# compromisos_parrafo1 = document.add_paragraph(
+#     "En el marco de la Norma Técnica para la implementación del mecanismo \
+# denominado Compromisos de Desempeño 2021, aprobada por Resolución \
+# Ministerial N° 042-2021-MINEDU y modificada por la Resolución Ministerial \
+# N° 160-2021-MINEDU, se han realizado transferencias de partidas a favor \
+# de las Unidades Ejecutoras de Educación del Gobierno Regional de ", style="List Bullet")
+# compromisos_parrafo1.add_run(region)
+# compromisos_parrafo1.add_run(" por la suma total de ")
+# compromisos_parrafo1.add_run(transferido_compromisos)
+# compromisos_parrafo1.add_run(" De dichos recursos, ")
+# compromisos_parrafo1.add_run(acciones_centrales_cdd)
+# compromisos_parrafo1.add_run(" corresponden a las acciones centrales, según el \
+#                              siguiente detalle")
+# compromisos_parrafo1.paragraph_format.alignment = docx.enum.text.WD_ALIGN_PARAGRAPH.JUSTIFY
 
 
-document.add_heading('4. Intervenciones pedagógicas ', level=1)
-pa6= document.add_paragraph('En las Unidades Ejecutoras de Educación de ', style='List Bullet')
-pa6.add_run(region)
-pa6.add_run(' vienen implementando ')
-pa6.add_run('XXX') #Reemplazar 
-pa6.add_run(' intervenciones y acciones pedagógicas en el Año 2021, en el marco de la Norma  \
-Técnica “Disposiciones para la implementación de las intervenciones y acciones pedagógicas  \
-del Ministerio de Educación en los Gobiernos Regionales y Lima Metropolitana en el Año Fiscal  \
-2021”, aprobada mediante RM N° 043-2021-MINEDU y modificada RM N° 159-2021-MINEDU.') 
-                       
-pa7= document.add_paragraph('Las Unidades Ejecutoras de Educación de ', style='List Bullet')
-pa7.add_run(region)
-pa7.add_run(' cuentan con S/ ')
-pa7.add_run('XXX') #Reemplazar 
-pa7.add_run(' en su Presupuesto Institucional Modificado (PIM), que permite cubrir el costo \
-para la implementación de las referidas intervenciones y acciones pedagógicas, por un monto de S/ ')
-pa7.add_run('XXX') #Reemplazar 
-pa7.add_run('. De dichos recursos, a fecha de corte del ')
-pa7.add_run(datetime.today().strftime('%d-%m-%y'))
-pa7.add_run(', se han ejecutado S/ ')
-pa7.add_run('XXX') #Reemplazar 
-pa7.add_run(', lo que equivale al ')
-pa7.add_run('XXX') #Reemplazar 
-pa7.add_run(' respecto al costo de los recursos.') 
-
-# Tablas1
-titu_tabla1= document.add_heading('Ejecución de intervenciones en ', level=1)
-titu_tabla1.add_run(region)
-
-data = pd.read_excel(here() / "input/gaaa.xlsx")
-
-t = document.add_table(data.shape[0]+1, data.shape[1])
-
-for j in range(data.shape[-1]):
-    t.cell(0,j).text = data.columns[j]
-
-# add the rest of the data frame
-for i in range(data.shape[0]):
-    for j in range(data.shape[-1]):
-        t.cell(i+1,j).text = str(data.values[i,j])
-        
-document.add_heading('5. Condiciones para la reapertura de IIEE: Mascarillas y protectores faciales', level=1)
-
-# Guardar output
-document.save(here() / "output/AM.docx")
+# # Guardar output
+# document.save(here() / "output/AM.docx")
