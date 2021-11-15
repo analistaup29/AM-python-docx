3de8f321b5d9e241ec13332869726b4badc125ab
analistaup29 <86805379+analistaup29@users.noreply.github.com>
2021-11-15 17:43:49 -0500

    Update 01_AM_unidad_ejecutora.py

--- scripts/01_AM_unidad_ejecutora.py
+++ scripts/01_AM_unidad_ejecutora.py
@@ -1,8 +1,23 @@
-#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Mon Nov 15 13:13:19 2021
 
-@author: bran
 """
+
+# Paquetes
+import pyodbc
+import pandas as pd
+
+##############################################################################
+# Cargamos data
+##############################################################################
+
+# Conectarse a BD UPP
+cnxn = pyodbc.connect(driver='{SQL Server}', server='10.200.2.45', database='db_territorial_upp',
+                      trusted_connection='yes')
+cursor = cnxn.cursor()
+
+# Cargamos data Disponibilidad
+query = "SELECT * FROM dbo.disponibilidad_presupuestal;"
+base_disponibilidad = pd.read_sql(query, cnxn)
+
 
